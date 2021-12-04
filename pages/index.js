658fcee99a6a7d1cf9e71dc9e2924cacc049bed6
szymon <szymonstuszek@gmail.com>
2021-12-04 07:14:49 +0100

    styling

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    static async getInitialProps() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return { campaigns };
+    }
+
+    renderCampaigns() {
+        const items = this.props.campaigns.map(
+            address => {
+                return {
+                    header: address,
+                    description: <a>View campaign</a>,
+                    fluid: true
+                }
+            }
+        );
+
+        return <Card.Group items={items} />;
     }
 
     render() {
-        return <div>Campaigns index</div>
+        return <div>
+              <link
+                async
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+              />
+            {this.renderCampaigns()}
+        </div>
     }
 }
 
