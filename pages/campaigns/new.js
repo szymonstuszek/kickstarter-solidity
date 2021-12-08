177df59d57b8ffb4d0ff538c6866dfbff48ced43
szymon <szymonstuszek@gmail.com>
2021-12-08 05:43:53 +0100

    form validation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: '',
+        loading: false
     };
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+        this.setState({loading: true, errorMessage: ''})
+
+        try{
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+            .createCampaign(this.state.minimumContribution)
+            .send({
+                from: accounts[0]
+            });
+        } catch(err) {
+            this.setState({ errorMessage: err.message })
+        }
+        this.setState({loading: false})
+    }
 
     render() {
         return (
             <Layout>
                 <h3>Create a campaign!</h3>
 
-                  <Form>
+                  <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                       <label>Minimum contribution</label>
                       <Input value={this.state.minimumContribution}
@@ -21,7 +42,9 @@ class CampaignNew extends Component {
                           labelPosition='right'
                       />
                     </Form.Field>
-                    <Button type='submit' color='yellow'>Create!</Button>
+
+                    <Message error header='Something went wrong...' content={this.state.errorMessage}/>
+                    <Button type='submit' color='yellow' loading={this.state.loading}>Create!</Button>
                   </Form>
 
             </Layout>
