ca1d983d9970d70f745429556322a4c5753d21b6
szymon <szymonstuszek@gmail.com>
2021-12-11 06:15:58 +0100

    Request form

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Link, Router } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestNew extends Component {
+    state = {
+        value: '',
+        description: '',
+        recipient: ''
+    };
+
+    static async getInitialProps(props) {
+        const { address } = props.query;
+
+        return { address };
+    }
+
+    onSubmit = async event => {
+        console.log('Submitting');
+    }
+
     render() {
         return (
-            <h3>New request</h3>
+            <Layout>
+                <h3>Create request</h3>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={event => this.setState({ description: event.target.value })}
+                         />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Value in Ether</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={event => this.setState({ value: event.target.value })}
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <label>Recipient</label>
+                        <Input
+                             value={this.state.recipient}
+                             onChange={event => this.setState({ recipient: event.target.value })}
+                         />
+                    </Form.Field>
+
+                    <Button primary>
+                        Create request!
+                    </Button>
+                </Form>
+            </Layout>
         );
     }
 }
