0445564bb4ae9ae0b4635414fbdeafdcc9c3adcc
szymon <szymonstuszek@gmail.com>
2021-12-11 06:01:47 +0100

    Create new request

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestIndex extends Component {
+    static async getInitialProps(props) {
+        const { address } = props.query;
+
+        return { address };
+    }
+
     render() {
         return (
-            <h3>Reuqest list</h3>
+            <Layout>
+                <h3>Requests</h3>
+                <Link route={`/campaigns/${this.props.address}/requests/new`}>
+                    <a>
+                        <Button primary>
+                            Add request
+                         </Button>
+                    </a>
+                </Link>
+            </Layout>
         );
     }
+
 }
 
 export default RequestIndex;
---
+++ pages/campaigns/requests/new.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class RequestNew extends Component {
+    render() {
+        return (
+            <h3>New request</h3>
+        );
+    }
+}
+
+export default RequestNew;
+
