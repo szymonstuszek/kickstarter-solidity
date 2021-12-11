88c0046619c045f2d70ae549ec2dacc16f3b6f1b
szymon <szymonstuszek@gmail.com>
2021-12-11 07:11:29 +0100

    Created table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
     static async getInitialProps(props) {
         const { address } = props.query;
 
-        return { address };
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
+
+        const requests = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element, index) => {
+                return campaign.methods.requests(index).call();
+            })
+        );
+
+        return { address, requests, requestCount };
     }
 
     render() {
+        const { Header, Row, HeaderCell, Body } = Table;
+
+
         return (
             <Layout>
                  <Link route={`/campaigns/${this.props.address}`}>
@@ -26,6 +39,19 @@ class RequestIndex extends Component {
                          </Button>
                     </a>
                 </Link>
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approval Count</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalized</HeaderCell>
+                        </Row>
+                    </Header>
+                </Table>
             </Layout>
         );
     }
