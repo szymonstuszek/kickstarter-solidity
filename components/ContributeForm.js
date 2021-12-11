caa4290265bc8e1467de2c5cc63e57b45b2c34dd
szymon <szymonstuszek@gmail.com>
2021-12-11 05:46:46 +0100

    Request list

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
     state = {
-        value: ''
+        value: '',
+        errorMessage: '',
+        loading: false
     };
 
     onSubmit = async event => {
@@ -13,18 +16,26 @@ class ContributeForm extends Component {
 
         const campaign = Campaign(this.props.address);
 
+        this.setState({ loading: true, errorMessage: '' });
+
         try {
             const accounts = await web3.eth.getAccounts();
             await campaign.methods.contribute().send({
                 from: accounts[0],
                 value: web3.utils.toWei(this.state.value, 'ether')
             });
-        } catch(err) {}
+
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+        } catch(err) {
+            this.setState({ errorMessage: err.message });
+        }
+
+        this.setState({ loading: false, value: '' });
     }
 
     render() {
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                 <Form.Field>
                     <label>Amount to contribute</label>
                     <Input
@@ -34,7 +45,9 @@ class ContributeForm extends Component {
                         labelPosition='right'
                     />
                 </Form.Field>
-                <Button primary>
+
+                <Message error header='Something went wrong...' content={this.state.errorMessage}/>
+                <Button loading={this.state.loading} primary>
                     Contribute!
                 </Button>
             </Form>
---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RequestIndex extends Component {
+    render() {
+        return (
+            <h3>Reuqest list</h3>
+        );
+    }
+}
+
+export default RequestIndex;
